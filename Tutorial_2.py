025fedecca119b476ac49d1374359adfe7535f96
Krisrc_QI <117539900+KrisrcQi@users.noreply.github.com>
2026-02-08 17:06:14 +0000

    technical analysis

--- Tutorial_2.py
+++ Tutorial_2.py
@@ -22,13 +22,32 @@ such as: adv20; 20 days average volume.
     ts_mean(volume, n) could compute n days volumes mean
 """
 
-### 1.1. VWAP (Volume-weighted average price)
+#### 1.1. VWAP (Volume-weighted average price)
+"""Volume weighted average price
+VWAP represents average daily stock price, which is weighted by trading volume. 
+Since, low trading volume might give false picture
+
+    VWAP = sum(price * volume)/sum(volume)
 """
+#### 1.2. PV-data Alpha idea:
+"""
+1. Momentum
+Assume that stock performed well which will continuely perform well. While stocks performed poorly, it will do so as well. 
+
+Momentum effect: typically appears over longer periods (several months or more)
+
+2. Reversion
+The hypothesis is that if something increases today, it will fall tomorrow. 
+But this hypothesis could apply on any indicator: price, volume, correlation between two things. 
+
+Reversion effect: appears over shorter periods (days or weeks).
+"""
+#### 1.3. Technical Anlaysis
+"""Technical analysis
+TA is an analyzed method that seeks to predict future price movements based on historical price and volume data.
+
 
 """
-
-
-
 
 ### 2. Fundamental Alphas
 """Fundamental Alphas
